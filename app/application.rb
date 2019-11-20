b0bf16800aa4642af3e1c43defb8be9a1bbc8406
Blythe Pollard <blytheepollard@gmail.com>
2019-11-20 23:50:08 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -28,9 +28,9 @@ class Application
       search_term = req.params["item"]
  
       if @@items.include?(search_term)
-        resp.write "#{search_term} is one of our items"
+        resp.write "added #{search_term}"
       else
-        resp.write "Couldn't find #{search_term}"
+        resp.write "We don't have that item"
       end
     
     else
